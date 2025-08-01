4984a92f6c194e6fcccb9a9c0792405cc9470a76
Sergei Kiselev <sergei_qa@icloud.com>
2025-08-01 15:14:42 +0200

    complited exercise3

--- Функции, словари, интерпретатор, файлы, модули/Файловый ввод, вывод/exercise3.py
+++ Функции, словари, интерпретатор, файлы, модули/Файловый ввод, вывод/exercise3.py
@@ -20,29 +20,53 @@ print('First;Second-1 Second-2;Third'.split(';'))
 # ['First', 'Second-1 Second-2', 'Third']
 '''
 
+
 from itertools import count
 
+
+# чтение из файла
 wholeList = ''
 with open('dataset_3363_5.txt') as inf:
     for line in inf:
         wholeList += line
 
+# Разделение на строки
 wholeList = wholeList.strip().split('\n')
 wholeList = [line.split(';') for line in wholeList]
 
+# вывод стредней оценки для каждого абитуриента
 average = []
-numb = 0
-counter = 0
-for row in range(len(wholeList)):
-    for column in range(1, len(wholeList) + 1):
+numb = 0  # сумма по трем предметам
+counter = 0  # счетчик
+for row in range(len(wholeList)):  # итерирование по строкам
+    for column in range(1, len(wholeList[row])): # итерирование внутри одной строки
         numb += int(wholeList[row][column])
         counter += 1
         if counter == 3:
-            average += [int(numb/3)]
+            average += [float(numb/3)]
             counter = 0
+            numb = 0
         elif column or row > len(wholeList)+1:
             continue
 
-print(average)
+math = 0  # сумма по трем предметам
+pith = 0  # сумма по трем предметам
+lang = 0  # сумма по трем предметам
+aver = []
 
+for row in range(len(wholeList)):  # итерирование по строкам
+    for column in range(1, len(wholeList[row])): # итерирование внутри одной строки
+        if column == 1:
+            math += int(wholeList[row][column])
+        elif column == 2:
+            pith += int(wholeList[row][column])
+        elif column == 3:
+            lang += int(wholeList[row][column])
 
+aver += [math/len(wholeList)]
+aver += [pith/len(wholeList)]
+aver += [lang/len(wholeList)]
+
+with open('dataset_3363_6.txt', 'w') as ouf:
+    ouf.write('\n'.join(map(str, average)) + '\n')
+    ouf.write(' '.join(map(str, aver)))
